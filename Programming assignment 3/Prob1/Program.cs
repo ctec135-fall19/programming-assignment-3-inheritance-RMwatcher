3aa0d29def1e0de7a76f5b49073d9a290a2abc84
Richard <r.mora@students.clark.edu>
2019-10-18 16:24:32 -0700

    Problem 1 complete for Richard Mora

---
+++ Programming assignment 3/Prob1/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Author: Richard Mora
+Date: 10/18/2019
+CTEC 135: MicroSoft Software Development with C#
+
+Module 4 Module 4, Programming Assignment 3, Problem 1
+
+    Inheritance Behavior:
+
+Using Class Diagramming Tool
+
+Using the Class Diagram to show the relationships between the base class
+HeadCheese, the two child classes Cheddar and Swiss and the grandchild
+classes WhiteCheddar and YellowCheddar for Cheddar and WholeSwiss and 
+SlicedSwiss for Swiss.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
